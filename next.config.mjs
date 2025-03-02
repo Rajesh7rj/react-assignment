49c90a0800d7e391115b630184b0b594cc6c63d2
rajesh <rajeshjanyani7@gmail.com>
2025-03-02 13:54:53 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/react-assignment/' : '',
-  basePath: isProd ? '/react-assignment' : '',
+  assetPrefix: isProd ? '/react-assignment`/' : '',
+  basePath: isProd ? '/react-assignment`' : '',
   output: 'export'
 };
 
