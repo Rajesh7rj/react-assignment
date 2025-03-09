d184ffafdf257484cf58d1ef2db6b38dfdb15756
rajesh <rajeshjanyani7@gmail.com>
2025-03-10 00:31:35 +0530

    FEAT: all section is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
+"use client"
+import React from 'react';
+import Banners from "@/components/Banners"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import RegisterNow from "@/components/RegisterNow"
+import Schedule from "@/components/Schedule"
+import ReserveStay from '@/components/ReserveStay'
+import Celebrations from "@/components/Celebrations"
 
-export default function Home() {
+
+export default function App() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     hello
+    <div>
+      <Header />
+      <Banners/>
+      <RegisterNow 
+         title="Let’s Rejoice The Arrival Of Unprecedented Grace In Our Lives!"
+         description="125 years ago, Param Krupalu Dev Shrimad Rajchandraji blessed the sacred land of Dharampur with His divine presence, forever sanctifying it."
+         ctaText="Register Now"
+      />
+      <Schedule />
+      <Celebrations/>
+      <ReserveStay
+        heading="Reserve Your Stay"
+        description="We warmly invite everyone to be part of this historic event. Family rooms will be available for stay till 26th Mar. To maximize accommodation for mumukshus, only shared rooms and hall dormitory options will be available from 27th Mar onwards. Apply now by clicking below."
+        buttonLabel="Reserve Your Stay"
+        buttonLink="https://hg.myhotelline.com/book.html?propertyId=SHR-HNV7-SION"
+      />
+      <Footer />
     </div>
   );
 }
+
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,18 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+// tailwind.config.js
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--foreground)", 
       },
+       height: {
+        '70vh': '70vh', 
+      }
     },
   },
   plugins: [],
-};
-export default config;
+}
